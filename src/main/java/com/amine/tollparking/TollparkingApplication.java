6159153e6221ca1886e74f19e8a439c318ff7ccd
Amine <a.a.boulifa@gmail.com>
2020-02-20 02:05:02 +0100

    Initial commit: code skeleton

---
+++ src/main/java/com/amine/tollparking/TollparkingApplication.java
@@ -0,0 +1,13 @@
+package com.amine.tollparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollparkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amine/tollparking/TollparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amine.tollparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
