db5332514c5f58731a2708c79bde770727641e6f
Amine <a.a.boulifa@gmail.com>
2020-02-26 13:59:52 +0100

    Repository tests + clientService tests

---
+++ src/test/java/com/amine/tollparking/repository/CarRepositoryTests.java
@@ -0,0 +1,32 @@
+package com.amine.tollparking.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
+
+@DataJpaTest
+@TestPropertySource(locations = "classpath:application-integrationtest.properties")
+public class CarRepositoryTests {
+	@Autowired
+	private DataSource dataSource;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private EntityManager entityManager;
+	@Autowired
+	private CarRepository carRepository;
+
+	@Test
+	void injectedComponentsAreNotNull() {
+		assertThat(dataSource).isNotNull();
+		assertThat(jdbcTemplate).isNotNull();
+		assertThat(entityManager).isNotNull();
+		assertThat(carRepository).isNotNull();
+	}
+}
---
+++ src/test/java/com/amine/tollparking/service/ClientServiceTests.java
@@ -0,0 +1,74 @@
+package com.amine.tollparking.service;
+
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.amine.tollparking.entity.Client;
+import com.amine.tollparking.errorhandler.ApiException;
+import com.amine.tollparking.repository.ClientRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class ClientServiceTests {
+	@Mock
+	private ClientRepository clientRepo;
+	
+	@InjectMocks
+	private ClientService clientService;
+	
+	private Client client;
+	
+	@BeforeEach
+	public void resetClient() {
+		client = new Client("Amine", "Boulifa", "Aminus", 100);
+	}
+	
+	@Test
+	public void clientEdition_WhenNewClientIsNull() {
+		//when(clientRepo.save(any(Client.class))).thenReturn(null);
+		ApiException thrown = Assertions.assertThrows(ApiException.class, () -> {
+			clientService.editClient(client, null);
+		    });
+		
+		assertEquals(thrown.getMessage(), "Error while editing existing client");
+	}
+	
+	@Test
+	public void clientEdition_WhenOldClientIsNull() {
+		ApiException thrown = Assertions.assertThrows(ApiException.class, () -> {
+			clientService.editClient(null, client);
+		    });
+		
+		assertEquals(thrown.getMessage(), "Error while editing existing client");
+	}
+	
+	@Test
+	public void clientEdition_WhenFieldIsMissing() {
+		Client newClient = new Client("Joe", "Derson", null, 100);
+		when(clientRepo.save(any(Client.class))).thenReturn(null);
+		
+		clientService.editClient(client, newClient);
+		
+		verify(clientRepo, times(1)).save(any(Client.class));
+	}
+	
+	@Test
+	public void clientEdition_WhenHappyPath() {
+		Client newClient = new Client("Joe", "Derson", "DEwi", 100);
+		when(clientRepo.save(any(Client.class))).thenReturn(null);
+		
+		clientService.editClient(client, newClient);
+		
+		verify(clientRepo, times(1)).save(any(Client.class));
+	}
+}
